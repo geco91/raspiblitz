58074c88b019948f6f80828e0e3632c22506cb48
rootzoll <christian@geektank.de>
2025-01-11 19:40:42 +0100

    adapt sources

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -12,12 +12,9 @@
 
 # debian sources
 REQUIRED_SOURCES=(
-  "deb http://deb.debian.org/debian bookworm main contrib non-free"
-  "deb-src http://deb.debian.org/debian bookworm main contrib non-free"
-  "deb http://security.debian.org/debian-security bookworm-security main contrib non-free"
-  "deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free"
-  "deb http://deb.debian.org/debian bookworm-updates main contrib non-free"
-  "deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free"
+  "deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware"
+  "deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware"
+  "deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware"
 )
 
 # set locale to en_US.UTF-8 on system & activate for this script
